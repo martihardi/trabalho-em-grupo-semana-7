b262c74bf52bd25a2e10a2d392c4be617b242b9d
Matheus <matheusmj12@hotmail.com>
2021-06-26 12:01:21 -0300

    first commit

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ exibirNumeros(2000);
 
 function exibirNumeros(limite){
     tabelaHTML = '<table class="a">';
-    for(var numero = 2; numero <= limite; numero++) {
+    for(var numero = 1; numero <= limite; numero++) {
         var primo = true;
 
         for(var divisor = 2; divisor < numero; divisor++){
             if(numero % divisor === 0) {
                 primo = false;
+                tabelaHTML += '<td>' + numero;
+                break;
             }
         }
 
         if(Boolean(primo)) {
-            tabelaHTML += "<td bgcolor='#00c10c'>" + numero;
+            tabelaHTML += "<td bgcolor='#6699FF'>" + numero;
         }
-
+       numero % 15 == 0 ? (tabelaHTML += '<tr>') : false;
     }
     tabelaHTML += '</table>';
     document.write(tabelaHTML)
